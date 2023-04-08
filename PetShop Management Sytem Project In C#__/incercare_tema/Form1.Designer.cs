d5f18d1e698b096d8fdd41d3cb6dfdfb7b76106c
ani-andrada <126412809+ani-andrada@users.noreply.github.com>
2023-04-08 21:41:32 +0300

    Add files via upload

---
+++ PetShop Management Sytem Project In C#__/incercare_tema/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace incercare_tema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1_adaugaCos = new System.Windows.Forms.Button();
+            this.listView1_cos = new System.Windows.Forms.ListView();
+            this.pictureBox1_imagineAnimal = new System.Windows.Forms.PictureBox();
+            this.textBox1_descriere = new System.Windows.Forms.TextBox();
+            this.radioButtonFeminin = new System.Windows.Forms.RadioButton();
+            this.radioButtonMasculin = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_imagineAnimal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.LightPink;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(20, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(260, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 108);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(520, 121);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1_adaugaCos
+            // 
+            this.button1_adaugaCos.BackColor = System.Drawing.Color.Pink;
+            this.button1_adaugaCos.Location = new System.Drawing.Point(609, 127);
+            this.button1_adaugaCos.Name = "button1_adaugaCos";
+            this.button1_adaugaCos.Size = new System.Drawing.Size(95, 33);
+            this.button1_adaugaCos.TabIndex = 3;
+            this.button1_adaugaCos.Text = "Adauga";
+            this.button1_adaugaCos.UseVisualStyleBackColor = false;
+            this.button1_adaugaCos.Click += new System.EventHandler(this.button1_adaugaCos_Click);
+            // 
+            // listView1_cos
+            // 
+            this.listView1_cos.BackColor = System.Drawing.Color.MistyRose;
+            this.listView1_cos.HideSelection = false;
+            this.listView1_cos.Location = new System.Drawing.Point(558, 256);
+            this.listView1_cos.Name = "listView1_cos";
+            this.listView1_cos.Size = new System.Drawing.Size(230, 133);
+            this.listView1_cos.TabIndex = 4;
+            this.listView1_cos.UseCompatibleStateImageBehavior = false;
+            this.listView1_cos.View = System.Windows.Forms.View.Details;
+            this.listView1_cos.SelectedIndexChanged += new System.EventHandler(this.listView1_cos_SelectedIndexChanged);
+            // 
+            // pictureBox1_imagineAnimal
+            // 
+            this.pictureBox1_imagineAnimal.BackColor = System.Drawing.Color.Snow;
+            this.pictureBox1_imagineAnimal.Location = new System.Drawing.Point(318, 246);
+            this.pictureBox1_imagineAnimal.Name = "pictureBox1_imagineAnimal";
+            this.pictureBox1_imagineAnimal.Size = new System.Drawing.Size(222, 203);
+            this.pictureBox1_imagineAnimal.TabIndex = 5;
+            this.pictureBox1_imagineAnimal.TabStop = false;
+            this.pictureBox1_imagineAnimal.Click += new System.EventHandler(this.pictureBox1_imagineAnimal_Click);
+            // 
+            // textBox1_descriere
+            // 
+            this.textBox1_descriere.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox1_descriere.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_descriere.Location = new System.Drawing.Point(20, 256);
+            this.textBox1_descriere.MaxLength = 30000;
+            this.textBox1_descriere.Multiline = true;
+            this.textBox1_descriere.Name = "textBox1_descriere";
+            this.textBox1_descriere.Size = new System.Drawing.Size(281, 163);
+            this.textBox1_descriere.TabIndex = 6;
+            this.textBox1_descriere.TextChanged += new System.EventHandler(this.textBox1_descriere_TextChanged);
+            // 
+            // radioButtonFeminin
+            // 
+            this.radioButtonFeminin.AutoSize = true;
+            this.radioButtonFeminin.Location = new System.Drawing.Point(329, 69);
+            this.radioButtonFeminin.Name = "radioButtonFeminin";
+            this.radioButtonFeminin.Size = new System.Drawing.Size(36, 20);
+            this.radioButtonFeminin.TabIndex = 7;
+            this.radioButtonFeminin.TabStop = true;
+            this.radioButtonFeminin.Text = "F";
+            this.radioButtonFeminin.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMasculin
+            // 
+            this.radioButtonMasculin.AutoSize = true;
+            this.radioButtonMasculin.Location = new System.Drawing.Point(400, 69);
+            this.radioButtonMasculin.Name = "radioButtonMasculin";
+            this.radioButtonMasculin.Size = new System.Drawing.Size(39, 20);
+            this.radioButtonMasculin.TabIndex = 8;
+            this.radioButtonMasculin.TabStop = true;
+            this.radioButtonMasculin.Text = "M";
+            this.radioButtonMasculin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Font = new System.Drawing.Font("High Tower Text", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label1.Location = new System.Drawing.Point(321, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 48);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "PetShop";
+            // 
+            // button1_save
+            // 
+            this.button1_save.Location = new System.Drawing.Point(610, 183);
+            this.button1_save.Name = "button1_save";
+            this.button1_save.Size = new System.Drawing.Size(93, 28);
+            this.button1_save.TabIndex = 10;
+            this.button1_save.Text = "Salveaza";
+            this.button1_save.UseVisualStyleBackColor = true;
+            this.button1_save.Click += new System.EventHandler(this.button1_save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(821, 478);
+            this.Controls.Add(this.button1_save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonMasculin);
+            this.Controls.Add(this.radioButtonFeminin);
+            this.Controls.Add(this.textBox1_descriere);
+            this.Controls.Add(this.pictureBox1_imagineAnimal);
+            this.Controls.Add(this.listView1_cos);
+            this.Controls.Add(this.button1_adaugaCos);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_imagineAnimal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1_adaugaCos;
+        private System.Windows.Forms.ListView listView1_cos;
+        private System.Windows.Forms.PictureBox pictureBox1_imagineAnimal;
+        private System.Windows.Forms.TextBox textBox1_descriere;
+        private System.Windows.Forms.RadioButton radioButtonFeminin;
+        private System.Windows.Forms.RadioButton radioButtonMasculin;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1_save;
+    }
+}
+
---
+++ PetShop Management Sytem Project In C#__/incercare_tema/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+
+namespace incercare_tema
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            comboBox1.Items.Add("Beagle");
+            comboBox1.Items.Add("Yorkshire");
+            comboBox1.Items.Add("British");
+            comboBox1.Items.Add("Sphynx");
+            comboBox1.Items.Add("Roborovski");
+            comboBox1.Items.Add("Syrian");
+            comboBox1.Items.Add("African Grey");
+            comboBox1.Items.Add("Macaws");
+
+             dt = new DataTable();
+            dt.Columns.Add("Nume");
+            dt.Columns.Add("Rasa");
+            dt.Columns.Add("Sex");
+            dt.Columns.Add("Varsta");
+            dt.Columns.Add("Pret(EUR)");
+            dt.Columns.Add("Descriere");
+            
+
+            dt.Rows.Add("Max", "Beagle","M", "2 ani","200", "El este Max, un beagle de 2 ani si 3 luni cu pedigree.Are nevoie de spațiu, iubire si o familie intelegatoare. Este cuminte,prietenoasa,jucaus si mereu plin de viata.");
+            dt.Rows.Add("Cody", "British", "M", "1 an", "150", " Cody un animal pe cât de inteligent, pe atât de activ și jucăuș, iar cel mic va fi foarte încântat să-și petreacă timpul în prezența ei. Această pisică este un adevărat argint viu.");
+            dt.Rows.Add("Kitty", "British", "F", "1 an","150", " Kitty un animal pe cât de inteligent, pe atât de activ și jucăuș, iar cel mic va fi foarte încântat să-și petreacă timpul în prezența ei. Această pisică este un adevărat argint viu.");
+            dt.Rows.Add("Yoyo", "Yorkshire", "F", "3 ani", "300", "Mica, agila și foarte loiala,Yoyo,se înțelege bine atât cu copiii, cât și cu adulții. Poate fi o minunată bonă cu patru picioare, însoțitoare și prietenă loială. Jucăușa și foarte vesela,se distinge prin bunătate și răbdare.");
+            dt.Rows.Add("Toby", "Yorkshire", "M", "4 ani","300", "Mic, agil și foarte de loial,Toby,se înțelege bine atât cu copiii, cât și cu adulții. Poate fi o minunată bonă cu patru picioare, însoțitoare și prietenă loială. Jucăuș și foarte vesel,se distinge prin bunătate și răbdare.");
+            dt.Rows.Add("Henry", "Macaws", "M", "2 ani", "100", "Henry una dintre cele mai prietenoase specii de papagali avand un comportament plin de viata si de energie. Sunt jucausi, zgomotosi si adeseori pot deveni distructivi daca nu sunt ingrijiti corespunzator. ");
+            dt.Rows.Add("Heidi", "Macaws", "F", "3 ani","120", "Heidi una dintre cele mai prietenoase specii de papagali avand un comportament plin de viata si de energie. Sunt jucausi, zgomotosi si adeseori pot deveni distructivi daca nu sunt ingrijiti corespunzator. ");
+            dt.Rows.Add("Goofy", "Syrian", "M", "3 ani", "70", "Goofy este denumit hamsterul cu ursuleț de pluș, deoarece are aspectul drăguț al jucăriței de peluză a unui copil, dar și pentru că este receptiv la a fi îmbrățișat și ținut de oamenii săi.");
+            dt.Rows.Add("Loly", "Syrian", "F", "2 ani","80", "Loly este denumita hamsterul cu ursuleț de pluș, deoarece are aspectul drăguț al jucăriei a unui copil, dar și pentru că este receptiv la a fi îmbrățișata și ținuta de oamenii săi.");
+            dt.Rows.Add("Mimi", "Sphynx", "F", "3 ani","400", "Aceasta pisicuta este ideală pentru familiile cu copii deoarece este un animal de companie activ, jucăuș, energic și prietenos cu cei mici.Mimi adoră să sară, să țopăie, este curioasă și agilă.");
+            dt.Rows.Add("Aldo", "Sphynx", "M", "2 ani", "450", "Aceasta pisicuta este perfecta pentru familiile cu copii deoarece este un animal de companie activ, jucăuș, energic și prietenos cu cei mici.Mimi adoră să sară, să țopăie, este spontana și agilă.");
+            dt.Rows.Add("Cora", "Roborovski", "F", "2 ani", "150", " Are picioarele mai lungi decat alte specii de hamsteri pitici. Este foarte adaptat la conditiile de desert, avand rinichi eficienti si produce foarte putina urina.");
+            dt.Rows.Add("Dean", "Roborovski", "M", "1 an", "150", " Are piciorusele mai lungi decat alte specii de hamsteri pitici. Este foarte adaptat la conditiile de desert, avand rinichi eficienti si produce foarte putina urina.");
+            dt.Rows.Add("Dino", "African Grey", "M", "1 an", "150", "Papagalul gri african Timneh, are un coloritul cenușiu închis, pe burtă cenușiu deschis, coada roșu - vișiniu închis, ciocul negru cu partea de sus maro deschis și picioarele negre - cenușii.\r\n");
+            dt.Rows.Add("Sindy", "African Grey", "F", "1 an", "150", "Sindy, are o culoare cenușiu închis, pe burtă cenușiu deschis, coada roșu - vișiniu închis, ciocul negru cu partea de sus maro deschis și picioarele negre - cenușii.\r\n");
+
+
+            listView1_cos.Columns.Add("Nume");
+            listView1_cos.Columns.Add("Pret(EUR)");
+
+            // setez dataTableu ca sursa pentru dataView
+            dataGridView1.DataSource = dt;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                string rasa = row["Rasa"].ToString();
+                if (!comboBox1.Items.Contains(rasa))
+                {
+                    comboBox1.Items.Add(rasa);
+                }
+            }
+
+            this.radioButtonMasculin.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+
+            this.radioButtonFeminin.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+
+
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            string filt = string.Empty;
+            if (radioButtonMasculin.Checked)
+            {
+                filt = "[Sex] = 'M'";
+            }
+            else if (radioButtonFeminin.Checked)
+            {
+                filt = "[Sex] = 'F'";
+            }
+
+            
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedValue = comboBox1.SelectedItem.ToString();
+                filt += string.IsNullOrEmpty(filt) ? string.Empty : " AND ";
+                filt += $"[Rasa] = '{selectedValue}'";
+            }
+
+            
+             (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = filt;
+
+           
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // fltrez DataTableu in functie de valoarea selectata în comboBox.
+            string selectedValue = comboBox1.SelectedItem.ToString();
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = $"[Rasa] = '{selectedValue}'";
+
+            // reinitializez coloanele și randurile dataView ului.
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            radioButton_CheckedChanged(sender, e);
+
+        }
+
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void CautaAnimale(string numeAnimal)
+        {
+          
+        }
+
+        private void button1_adaugaCos_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow dataGridViewRow = dataGridView1.SelectedRows[0];
+
+                string nume = dataGridViewRow.Cells[0].Value.ToString();
+                Double pret = Convert.ToDouble(dataGridViewRow.Cells[4].Value);
+
+                ListViewItem item = new ListViewItem(nume);
+                item.SubItems.Add(pret.ToString());
+                listView1_cos.Items.Add(item);
+                listView1_cos.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            }
+            else
+            {
+                MessageBox.Show("Selectați un rând din tabelul de animale pentru a-l adăuga în coșul de cumpărături.");
+            }
+
+        }
+        private void listView1_cos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_imagineAnimal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+            string nume = row.Cells[0].Value.ToString();      
+            pictureBox1_imagineAnimal.Image = Image.FromFile("C:\\Users\\Vasilica\\Desktop\\C#_II\\incercare_tema\\incercare_tema\\bin\\Debug\\" + nume + ".jpg");
+            pictureBox1_imagineAnimal.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            string descriere = row.Cells[5].Value.ToString();
+            textBox1_descriere.Text = descriere;               
+        }
+
+        private void textBox1_descriere_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_save_Click(object sender, EventArgs e)
+        {
+
+            using (StreamWriter sw = new StreamWriter("cos.txt"))
+            {
+                foreach (ListViewItem item in listView1_cos.Items)
+                {
+                    sw.WriteLine($"{item.SubItems[0].Text} - {item.SubItems[1].Text}");
+                }
+
+                int suma = 0;
+               
+                for (int i = 0; i < listView1_cos.Items.Count; i++)
+                {
+                    int sum;
+                    if (int.TryParse(listView1_cos.Items[i].SubItems[1].Text, out sum))
+                {
+                    suma += sum;
+                }
+                else
+                {
+                    MessageBox.Show($"Valoarea {listView1_cos.Items[i].SubItems[1].Text} nu poate fi convertita într-un nr intreg.", "eroare");
+                }
+                }
+                sw.WriteLine($"Totalul este: {suma.ToString("N2")} EURO");
+               
+
+            }
+
+        }
+    }
+    }
+
